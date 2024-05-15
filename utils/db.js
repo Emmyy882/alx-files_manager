21c03e84708ea5b511c27d3e6cd5f09cbfdcdce6
Emmyy882 <emmaagbaje3@gmail.com>
2024-05-15 03:02:21 -0700

    update

--- utils/db.js
+++ utils/db.js
@@ -1,54 +1,10 @@
 import MongoClient from 'mongodb';
 
 class DBClient {
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
+  constructor() {
+    const host = proces.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
 
-        const url = `mongodb://${host}:${port}`;
-        this.client = new MongoClient(url, { useUnifiedTopology: true });
-        this.dbName = database;
-    }
-
-    async connect() {
-        try {
-            await this.client.connect();
-            this.db = this.client.db(this.dbName);
-        } catch (error) {
-            console.error('Error connecting to MongoDB:', error);
-            throw error;
-        }
-    }
-
-    isAlive() {
-        return !!this.client && this.client.isConnected();
-    }
-
-    async nbUsers() {
-        try {
-            const usersCollection = this.db.collection('users');
-            const count = await usersCollection.countDocuments();
-            return count;
-        } catch (error) {
-            console.error('Error counting users:', error);
-            throw error;
-        }
-    }
-
-    async nbFiles() {
-        try {
-            const filesCollection = this.db.collection('files');
-            const count = await filesCollection.countDocuments();
-            return count;
-        } catch (error) {
-            console.error('Error counting files:', error);
-            throw error;
-        }
-    }
-}
-
-// Create and export an instance of DBClient
-const dbClient = new DBClient();
-module.exports = dbClient;
-
+    const uri = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(uri, { useUnifiedTopology = true });
